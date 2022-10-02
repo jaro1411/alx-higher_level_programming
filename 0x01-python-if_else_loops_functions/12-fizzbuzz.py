03f7ee9aeaf3f2cc5673a9555b99e776bde619bd
jaro1411 <jaredobare91@gmail.com>
2022-10-03 01:05:56 +0300

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,4 +7,4 @@ for number in range(1, 101):
     elif number % 3 == 0:
         print('Fizz', end=' ')
     else:
-        print('{}'.format(number), end=' ')
+        print('{} '.format(number), end='')
